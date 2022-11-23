3ca868ac1fa79fba566cbeb3a430e6279bafab41
Mercy Boma <mercy.boma35@yahoo.com>
2022-11-23 07:15:36 +0100

    Add to-do-App project

---
+++ To-do App.swiftpm/Model/Taskfilter.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+enum TaskFilter: String
+{
+    static var allFilters: [TaskFilter]
+    {
+        return [.NonCompleted,.Completed,.OverDue,.All]
+    }
+    
+    case All = "All"
+    case NonCompleted = "To Do"
+    case Completed = "Completed"
+    case OverDue = "Overdue"
+}
---
+++ To-do App.swiftpm/Todo list app/Todo list.swift
@@ -0,0 +1,19 @@
+
+import SwiftUI
+
+@main
+struct TodoListAppTutorialApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            
+            let context = persistenceController.container.viewContext
+            let dateHolder = DateHolder(context)
+            
+            TaskListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(dateHolder)
+        }
+    }
+}
